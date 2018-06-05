f0f9b6a2d12d61842381a23a855e95748204a9fa
Austin <austin1911remo@gmail.com>
2018-06-04 23:30:24 -0400

    Linking Between Routes S9L79

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,19 +28,34 @@ const HelpPage = () => (
     </div>
 );
 
+// Link is used for client-side routing, it doesn't refresh the page, instead it
+// just swaps things out on the fly and calls ReactDOM.render again
 const NotFoundPage = () => (
     <div>
-        404!
+        404 - <Link to="/">Go Home</Link>
     </div>
 );
+
+// activeClassName only gets applied when the page is the one clicked on
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
+)
 
 // path = where we want to show something
 // component = what we want to render, when we match the route
 // exact = because it doesn't look for exact matches inherently, it will serve up anything with "/"
-// <Switch> --> traverses inorder and stops when the correct path is found. If none is found it'll display
+// <Switch> --> traverses from top to bottom and stops when the correct path is found. If none is found it'll display
 // the last one which is the 404 page
 const routes = (
     <BrowserRouter>
+        <div>
+            <Header />
         <Switch>
             <Route path="/"  component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
@@ -48,6 +63,7 @@ const routes = (
             <Route path="/help" component={HelpPage} />
             <Route component={NotFoundPage} />
         </Switch>
+        </div>
     </BrowserRouter>
 );
 
