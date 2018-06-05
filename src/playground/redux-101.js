4d989156062d9e7b8077236481eff2967356490d
Austin <austin1911remo@gmail.com>
2018-06-05 15:56:46 -0400

    Dispatching Actions S10L86

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,9 +2,42 @@ import {createStore} from 'redux';
 
 // Creates a store, expects a function as an argument
 // count is the default state object
-const store = createStore((state = { count: 0 }) => {
-    return state;
+// action is second param
+const store = createStore((state = { count: 0 }, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return {count: state.count + 1};
+        case 'DECREMENT':
+            return {count: state.count - 1};
+        case 'RESET':
+            return {count: 0};
+        default:
+            return state;
+    }
 });
 
 // Returns current state object
-store.getState();
+console.log(store.getState());
+
+// Actions - an object that gets sent to the store
+// walk, stop walking, work, stop_working <-- all actions that change a state
+
+// Increment the count
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+
+console.log(store.getState());
