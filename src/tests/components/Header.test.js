54a75ceddd4db340abc57d93542f0677033971e0
Austin <austin1911remo@gmail.com>
2018-06-17 00:49:03 -0400

    Snapshot Testing S12L118

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,10 @@
+// ShallowRendering --> only renders the given component
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import React from 'react';
+import Header from '../../components/Header';
+
+test('Should render Header correctly', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
