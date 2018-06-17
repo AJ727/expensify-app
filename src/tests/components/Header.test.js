33e0052ef0dd1dc95da7157926bce3b5353ac8f1
Austin <austin1911remo@gmail.com>
2018-06-17 01:16:41 -0400

    Enzyme S12L119

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,14 @@
-// ShallowRendering --> only renders the given component
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Header from '../../components/Header';
 
+// .find is similar to document.querySelectorAll()
 test('Should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+    //expect(wrapper.find('h1').text()).toBe('Expensify');
+    
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
