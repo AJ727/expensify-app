51764e6052aae5c5147bb866b22afc3188c59650
Austin <austin1911remo@gmail.com>
2018-06-21 22:57:07 -0400

    New Feature Workflow S13L138

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
-// We grab the 5 props below using destructuring
+// We grab the 4 props below using destructuring
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')} 
+            - 
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 )
 
