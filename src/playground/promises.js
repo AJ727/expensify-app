24ab0c06e92c4d952bfbfca5b2502780398b8057
Austin <austin1911remo@gmail.com>
2018-06-23 12:21:11 -0400

    ES6 Promises S14L144

---
+++ src/playground/promises.js
@@ -0,0 +1,20 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve({
+        //     name: 'aj',
+        //     age: 23
+        // });
+        reject('it went wrong!');
+    }, 4500)
+});
+
+console.log('before');
+
+// data is the argument in resolve above resolve(this stuff here)
+promise.then((data) => {
+    console.log('1', data);
+}).catch((error) => {
+    console.log(error, 'error')
+});
+
+console.log('after');
