1775b630eed7d032d51a0b8633ee492f745f20a7
Austin <austin1911remo@gmail.com>
2018-06-24 00:18:35 -0400

    Fetching Data from Firebase S14L148

--- src/playground/firebaseTemp.js
+++ src/playground/firebaseTemp.js
@@ -8,33 +8,71 @@ firebase.initializeApp(config);
 // Connect to database
 const database = firebase.database();
 
-// ref = reference (in this case we are referencing root)
-// .then() = uses Promise(set() returns a Promise) to run a function if the change is successful
-database.ref().set({
-    name: 'AJ',
-    age: 23,
-    stressLevel: 6, 
-    job: {
-        title: 'Software dev',
-        company: 'Google'
-    },
-    location: {
-        city: 'Tampa',
-        country: 'USA'
-    }
-// If successful
-}).then(() => {
-    console.log('Data is saved');
-// If failed
-}).catch((e) => {
-    console.log('FAILED:', e);
+// 'value' is event type
+const onValueChange = database.ref().on('value', (snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+}, (e) => {
+    console.log('Error: ', e);
 });
 
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon',
-    'location/city': 'Seattle'
-});
+// // .on() listens for something over and over again
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (e) => {
+//     console.log('Error: ', e);
+// });
+
+// setTimeout(() => {
+//     database.ref('age').set(39);
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('age').set(40);
+// }, 10500);
+
+// // .once() returns a Promise, returns data a single time
+// database.ref()
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log(e);
+//     });
+
+// ref = reference (in this case we are referencing root)
+// .then() = uses Promise(set() returns a Promise) to run a function if the change is successful
+// database.ref().set({
+//     name: 'AJ',
+//     age: 23,
+//     stressLevel: 6, 
+//     job: {
+//         title: 'Software dev',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Tampa',
+//         country: 'USA'
+//     }
+// // If successful
+// }).then(() => {
+//     console.log('Data is saved');
+// // If failed
+// }).catch((e) => {
+//     console.log('FAILED:', e);
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// });
 
 // We must use the 'location/city' key in order to keep country
 // database.ref().update({
