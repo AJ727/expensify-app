26145a87a696186d3d44ccc335dd8550f2f143c3
Austin <austin1911remo@gmail.com>
2018-06-30 13:28:07 -0400

    Logging Out S16L163

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {NavLink} from 'react-router-dom';
+import { startLogout } from '../actions/auth';
 
 // activeClassName only gets applied when the page is the one clicked on
-const Header = () => (
+export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <button onClick={startLogout}>Logout</button>
     </header>
-)
+);
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
