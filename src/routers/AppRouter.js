217f3ec876ad67c7e66f8e8873b79f6a4c2b6704
Austin <austin1911remo@gmail.com>
2018-07-01 01:38:24 -0400

    Public Only Routes S16L167

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 // Stateless functional React Component
 // path = where we want to show something
@@ -22,11 +22,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
         <Switch>
-            <Route path="/" component={LoginPage} exact={true} />
+            <PublicRoute path="/" component={LoginPage} exact={true} />
             <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
             <PrivateRoute path="/create" component={AddExpensePage} />
             <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
             <Route component={NotFoundPage} />
         </Switch>
         </div>
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import Header from '../components/Header';
 
 // Unconnected Stateless Functional Component
-// ...rest gets the rest of the props (so not isAuth or component)
-export const PrivateRoute = ({ isAuthenticated, component: Component, ...rest}) => (
+export const PublicRoute = ({ isAuthenticated, component: Component, ...rest}) => (
     <Route {...rest} component={(props) => (
         isAuthenticated ? (
-            <div>
-                <Header />
-                <Component {...props} />
-            </div>
+            <Redirect to="/dashboard" />
         ) : (
-            <Redirect to="/" />
+            <Component {...props} />    
         )
     )}/>
 );
@@ -23,4 +18,4 @@ const mapStateToProps = (state) => ({
     isAuthenticated: !!state.auth.uid
 });
 
-export default connect(mapStateToProps)(PrivateRoute);
+export default connect(mapStateToProps)(PublicRoute);
